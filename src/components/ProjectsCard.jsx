b8fdd124a5a3d2ad5184e1c6016c1853620a8ad5
ckaynatividad <natividad.ckay@gmail.com>
2023-12-04 19:05:23 -0800

    test git commit

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function ProjectsCard({ projects }) {
   return (
-    <div className=" h-48">
+    <div>
       {projects.map((proj) => (
-        <div className="flex text-xs my-8 bg-neutral-800 rounded-lg">
-          <img className="w-1/3 h-1/3" src={proj.img} />
+        <div className="flex text-xs my-4 bg-neutral-800 rounded-lg">
+          {/* <img className="w-1/3 h-1/3" src={proj.img} /> */}
           <div className="flex flex-col p-4">
             <p className="text-xl">{proj.title}</p>
             <p className="text-gray-400">{proj.desc}</p>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProjectsCard from "../components/ProjectsCard";
 import ThingsCard from "../components/ThingsCard";
 import { projectsData } from "../services/projectsData";
 import { thingsData } from "../services/thingsData";
+import Projects from "./Projects";
+import Websites from "./Websites";
 
 export default function Home() {
   return (
     <div className="justify-right text-white w-2/5 mt-10 m-auto">
       <h1 className="text-3xl font-bold my-5">Hi, I'm Charysma!</h1>
-      <p className="mb-5">I'm a software developer.</p>
 
-      <ThingsCard things={thingsData} />
-      <ProjectsCard projects={projectsData} />
+      <p className="mb-5">
+        I'm a software developer. Sometimes I make{" "}
+        <a className="underline" href="">
+          art
+        </a>
+        .
+      </p>
+      <div className="flex flex-col">
+        <Projects />
+        <Websites />
+      </div>
     </div>
   );
 }
